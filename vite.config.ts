96c3b7b3193a989c1721e765a168a8080790c42f
Joel Kasisi <joelkasisi10@gmail.com>
2024-11-06 13:38:48 +0200

    fix actions

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/todos/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    sourcemap: true,
+  }
 })
